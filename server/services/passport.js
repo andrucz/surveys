a1ffd924c18dd60aefdf8e9ca54ba7076187850a
Acácio Andruczewicz <andrucz@gmail.com>
2018-03-03 11:07:33 -0500

    user persistence

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,18 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
+const User = mongoose.model('users');
+
+const googleStrategyConfig = {
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+};
+
+const persistUser = (accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id })
+    .then(exisingUser => {
+      if (!exisingUser) {
+        const user = new User({
+          googleId: profile.id
+        });
+        user.save();
+      }
+    });
+};
+
+const googleStrategy = new GoogleStrategy(googleStrategyConfig, persistUser);
+passport.use(googleStrategy);
