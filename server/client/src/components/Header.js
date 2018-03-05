e27361eebc5d44fb5eea2649766670390d3711ae
Acácio Andruczewicz <andrucz@gmail.com>
2018-03-04 19:50:53 -0500

    logout button

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
@@ -10,9 +28,7 @@ class Header extends Component {
             Surveys
           </a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
