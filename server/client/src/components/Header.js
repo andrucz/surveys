50cd5f7656a7d7b0ddf0166736bcd2c0aa9d33c3
Acácio Andruczewicz <andrucz@gmail.com>
2018-03-04 21:59:52 -0500

    landing page and brand logo link

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; 
 import { connect } from 'react-redux';
 
 class Header extends Component {
@@ -24,9 +25,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : ''}
+            className="left brand-logo"
+          >
             Surveys
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
